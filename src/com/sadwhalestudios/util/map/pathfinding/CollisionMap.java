6497961a355acb1eea89477f5d9a7341ae87f27e
Ashley Davies-Lyons <wiltedchameleon@gmail.com>
2013-07-02 13:16:10 +0100

    AI patch 1 [Pathfinding patch 1]

---
+++ src/com/sadwhalestudios/util/map/pathfinding/CollisionMap.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sadwhalestudios.util.map.pathfinding;
+
+/**
+ *
+ * @author
+ * Ashley
+ */
+public class CollisionMap {
+    boolean map[][];
+}
